6e795668e406f198213b004de01c25978cd66403
Saban-94 <hsaban2025@gmail.com>
2026-02-27 03:31:50 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+        },
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
